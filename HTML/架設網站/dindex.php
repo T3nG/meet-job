08b38bec589d7d36b18d35f6f71351ffc1655fd9
T3nG <dengfixanros@gmail.com>
2022-12-15 01:11:38 +0800

    20220903_HTML

---
+++ HTML/架設網站/dindex.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>動態</title>
+  </head>
+  <body style='color:blue'>
+    <center>
+    <?php
+	  
+	  for($x=1;$x<=6;$x++)
+	  {
+	    echo "<h" . $x . ">" . "Hi, Everyone" . "</h" . $x . ">";		
+	  }
+	?>
+	</center>
+  </body>
+  
+</html>
